eccb9c2ce8731d14bcee671c5ad5a403a3458e70
Ildar <tag.ildar@gmail.com>
2019-08-03 01:13:30 +0300

    Пример реализации адаптера

---
+++ Adapter/WarriorUnit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+	class WarriorUnit : IUnit
+	{
+		private Warrior warrior;
+
+		public WarriorUnit(Warrior warrior)
+		{
+			this.warrior = warrior;
+		}
+
+		public void DealDamage()
+		{
+			warrior.Attack();
+		}
+	}
+}
