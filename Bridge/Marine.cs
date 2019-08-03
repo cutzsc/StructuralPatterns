dca7ba2ee9faa2b0cc0fa90689f0aa8a524052cc
Ildar <tag.ildar@gmail.com>
2019-08-03 11:49:38 +0300

    Пример реализации моста

---
+++ Bridge/Marine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+	class Marine : Unit
+	{
+		public Marine(IWeapon weapon)
+			: base(weapon) { }
+
+		public override void Savage()
+		{
+			Console.WriteLine("Who's next.");
+		}
+	}
+}
--- Bridge/Program.cs
+++ Bridge/Program.cs
@@ -10,6 +10,21 @@ namespace Bridge
 	{
 		static void Main(string[] args)
 		{
+			Unit cowboy = new Cowboy(new Revolver());
+			Unit marine = new Marine(new M16());
+
+			marine.Attack();
+			marine.Weapon = new Revolver();
+			marine.Savage();
+			marine.Attack();
+
+			Console.WriteLine("\n\n");
+			cowboy.Attack();
+			cowboy.Weapon = new M16();
+			cowboy.Savage();
+			cowboy.Attack();
+
+			Console.ReadLine();
 		}
 	}
 }
