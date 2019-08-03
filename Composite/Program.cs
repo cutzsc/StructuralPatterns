a33b492dc702a827551a51c32b97afa0383f4be6
Ildar <tag.ildar@gmail.com>
2019-08-03 12:26:31 +0300

    Реализация паттерна Composite

---
+++ Composite/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Group gr1 = new Group("3");
+			Marine m1 = new Marine();
+			Marine m2 = new Marine();
+			Marauder md1 = new Marauder();
+
+			m1.Select();
+			gr1.Add(m1);
+			gr1.Select();
+
+			md1.Select();
+			gr1.Add(md1);
+			gr1.Select();
+
+			gr1.Add(m2);
+			gr1.Select();
+
+			gr1.Move();
+
+			Console.ReadLine();
+		}
+	}
+}
