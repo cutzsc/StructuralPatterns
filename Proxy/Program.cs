7ee9f6a7a3fdcdb5ccbd8b8b7bf75bdeeae0ca5a
Ildar <tag.ildar@gmail.com>
2019-08-04 23:20:07 +0300

    Реализация паттерна Proxy

--- Proxy/Program.cs
+++ Proxy/Program.cs
@@ -10,7 +10,19 @@ namespace Proxy
 	{
 		static void Main(string[] args)
 		{
+			using (IBook book = new BookStoreProxy())
+			{
+				Page page = book.GetPage(1);
+				Console.WriteLine(page.Text);
 
+				page = book.GetPage(2);
+				Console.WriteLine(page.Text);
+
+				page = book.GetPage(1);
+				Console.WriteLine(page.Text);
+			}
+
+			Console.ReadLine();
 		}
 	}
 }
