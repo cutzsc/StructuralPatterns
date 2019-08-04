18aaf2e4cdb78caff608f5b5c2a80e089d5a2fc1
Ildar <tag.ildar@gmail.com>
2019-08-04 13:51:50 +0300

    Реализация паттерна Flyweight

--- Flyweight/Program.cs
+++ Flyweight/Program.cs
@@ -10,6 +10,20 @@ namespace Flyweight
 	{
 		static void Main(string[] args)
 		{
+
+			TreeFactory fac = new TreeFactory();
+
+			Tree t1 = fac.GetTree(typeof(Fir));
+			t1.Draw(new Vector(1, 1, 1));
+			Tree t2 = fac.GetTree(typeof(Fir));
+			t2.Draw(new Vector(2, 2, 2));
+			Tree t3 = fac.GetTree(typeof(Fir));
+			t3.Draw(new Vector(3, 3, 3));
+
+			t1.Print();
+			t2.Print();
+
+			Console.ReadLine();
 		}
 	}
 }
---
+++ Flyweight/Tree.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flyweight
+{
+	class Tree
+	{
+		protected TreeModel model;
+
+		protected Vector position;
+
+		public Tree(TreeModel model)
+		{
+			this.model = model;
+		}
+
+		public virtual void Draw(Vector position)
+		{
+			this.position = position;
+			Console.WriteLine($"Отрисовано дерево {model.Name} в позиции {position.x}, {position.y}, {position.z}");
+		}
+
+		public virtual void Print()
+		{
+			Console.WriteLine($"Tree position: {position}");
+		}
+	}
+}
